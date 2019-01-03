7018abf5262c600196e9768ac4ed3ac927f8d957
Your Name <you@example.com>
2019-01-03 18:18:09 +0800

    2nd commit

--- vmcreate.py
+++ vmcreate.py
@@ -6,7 +6,7 @@ ec2 = boto3.resource('ec2')
 instances = ec2.create_instances(
      ImageId='ami-0ad620533c8392bb3',
      MinCount=1,
-     MaxCount=2,
+     MaxCount=1,
      InstanceType='t2.micro',
      KeyName='neeeew'
  )
